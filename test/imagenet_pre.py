eb7c4fb5eedc06e606aa801c6dd6f6b5787be35d
bigint406 <1084973153@qq.com>
2024-12-18 18:24:23 +0800

    big

--- test/imagenet_pre.py
+++ test/imagenet_pre.py
@@ -1,9 +1,10 @@
 # imagenet数据集预处理
 import os
+import tarfile
 import scipy.io
 import shutil
 
-def move_valimg(val_dir='/mnt/data/zs/samba/datasets/imagenet-1k/val', devkit_dir='/mnt/data/zs/samba/datasets/imagenet-1k/ILSVRC2012_devkit_t12'):
+def move_val_img(val_dir='/mnt/data/zs/samba/datasets/imagenet-1k/val', devkit_dir='/mnt/data/zs/samba/datasets/imagenet-1k/ILSVRC2012_devkit_t12'):
     """
     move valimg to correspongding folders.
     val_id(start from 1) -> ILSVRC_ID(start from 1) -> WIND
@@ -38,5 +39,26 @@ def move_valimg(val_dir='/mnt/data/zs/samba/datasets/imagenet-1k/val', devkit_di
             os.mkdir(output_dir)
         shutil.move(os.path.join(root, filename), os.path.join(output_dir, filename))
 
+def untar_train_img():
+    folder_path = "/mnt/data/zs/samba/datasets/imagenet-1k/train"
+
+    # 遍历文件夹中的所有.tar文件
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".tar"):
+            # 创建相同名称的文件夹
+            folder_name = os.path.splitext(file_name)[0]
+            folder_path_new = os.path.join(folder_path, folder_name)
+            os.makedirs(folder_path_new, exist_ok=True)
+
+            # 解压.tar文件到新文件夹
+            tar_path = os.path.join(folder_path, file_name)
+            with tarfile.open(tar_path, "r") as tar:
+                tar.extractall(path=folder_path_new)
+
+            # 删除原始.tar文件
+            os.remove(tar_path)
+            # break
+
 if __name__ == '__main__':
-    move_valimg()
+    # move_val_img()
+    untar_train_img()
